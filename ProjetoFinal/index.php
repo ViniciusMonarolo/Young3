1cc5cdc90f92d4900ad55be69dcb3e4882a703fd
ViniciusMonarolo <115747968+ViniciusMonarolo@users.noreply.github.com>
2023-12-02 12:26:52 -0300

    Update index.php

--- ProjetoFinal/index.php
+++ ProjetoFinal/index.php
@@ -1,49 +1,42 @@
-<?php 
+<?php
 include('conexao.php');
 
-if(isset($_POST['email']) || isset($_POST['password']))
-{
-    
-    if(strlen($_POST['email']) == 0)
-    {
-        echo "Prencha o seu e-mail!!!";
-    }
-    else if (strlen($_POST['password']) == 0)
-    {
-        echo "Preencha a sua senha!!!";
-    }
-    else
-    {
-        $email = $msqli->real_escape_string($_POST['email']);
-        $pw = $msqli->real_escape_string($_POST['password']);
+if(isset($_POST['email']) || isset($_POST['senha'])) {
 
-        //comando sql para pegar e ver se o user existe
-        $sql_cod = "SELECT * FROM usuario WHERE email= '$email' AND senha= '$pw'";
-        //lança o comando
-        $sql_query = $msqli->query($sql_cod) or die("Falha de execução do código SQL:". $msqli->error);
-        
+    if(strlen($_POST['email']) == 0) {
+        echo "Preencha seu e-mail";
+    } else if(strlen($_POST['senha']) == 0) {
+        echo "Preencha sua senha";
+    } else {
+
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
         $quantidade = $sql_query->num_rows;
 
-        if ($quantidade == 1)
-        {
+        if($quantidade == 1) {
+            
             $usuario = $sql_query->fetch_assoc();
 
-            if(!isset($_SESSION))
-            {
+            if(!isset($_SESSION)) {
                 session_start();
             }
+
             $_SESSION['id'] = $usuario['id'];
             $_SESSION['nome'] = $usuario['nome'];
 
             header("Location: painel.php");
-        }
-        else{
-            echo "Falaha ao logar!!!";
-        }
-    }
-    
-}
 
+        } else {
+            echo "Falha ao logar! E-mail ou senha incorretos";
+        }
+
+    }
+
+}
 ?>
 
 
@@ -101,9 +94,9 @@ cursor: pointer;
 <body>
 <div class="form-container">
     <img class="profile-pic" src="img/guest.png" alt="Foto de Perfil">
-    <form action="info.php" method="get">
+    <form action="" method="Post">
       <input class="input-field" type="email" name="email" placeholder="Email" required>
-      <input class="input-field" type="password" name="pass" placeholder="Senha" required>
+      <input class="input-field" type="password" name="senha" placeholder="Senha" required>
       <button class="submit-button" value="Enviar" type="submit">Enviar</button>
     </form>
   </div>
